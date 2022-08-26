64ce8de74281025ee5eacdad81e142dfcc14fe63
Denis Tavabilov <dagger1233@gmail.com>
2022-08-27 02:14:55 +0500

    end header!!

--- src/components/Main/Main.tsx
+++ src/components/Main/Main.tsx
@@ -2,6 +2,10 @@ import React from 'react';
 import commonStyle from '../common/CommonStyle.module.scss'
 import s from './Main.module.scss'
 import photo from '../../assets/img/i.png'
+import {ReactComponent as LinkedIn} from 'assets/img/socialMedia/linkedin.svg'
+import {ReactComponent as Github} from 'assets/img/socialMedia/github.svg'
+import {ReactComponent as Telegram} from 'assets/img/socialMedia/telegram.svg'
+import {ReactComponent as Codewars} from 'assets/img/socialMedia/codewars.svg'
 
 export const Main = () => {
     return (
@@ -12,7 +16,22 @@ export const Main = () => {
                         <img src={`${photo}`} alt="logo"/>
                     </div>
                     <h1 className={s.main__author}>Denis Tavabilov</h1>
-                    <p className={s.main__text}>I'm frontend developer</p>
+                    <p className={s.main__text}>I'm a frontend developer</p>
+                    <div className={s.main__socialMedia}>
+                        <span className={s.main__line}></span>
+                        <a href={'$'}>
+                            <LinkedIn className={s.main__icon}/>
+                        </a>
+                        <a href="#">
+                            <Github className={s.main__icon}/>
+                        </a>
+                        <a href="$">
+                            <Telegram className={s.main__icon}/>
+                        </a>
+                        <a href="$">
+                            <Codewars className={s.main__icon}/>
+                        </a>
+                    </div>
                 </div>
             </div>
         </section>
--- src/components/Navigation/Navigation.tsx
+++ src/components/Navigation/Navigation.tsx
@@ -2,30 +2,34 @@ import React, {useState} from 'react';
 import s from './Navigation.module.scss'
 
 export const Navigation = () => {
+    const [isClickBurger, setIsClickBurger] = useState(false)
+
+    const onBurgerClick = () => setIsClickBurger(!isClickBurger)
+    const onLinkClick = () => setIsClickBurger(false)
 
     return (
         <>
             <nav className={`${s.nav}`}>
-                <ul className={`${s.menu}`} >
+                <ul className={`${s.menu} ${isClickBurger ? s.right : ""}`}>
                     <li className={s.menu__item}>
-                        <a href="#" className={s.menu__link}>Main</a>
+                        <a href="#" className={s.menu__link} onClick={onLinkClick}>Main</a>
                     </li>
                     <li className={s.menu__item}>
-                        <a href="#" className={s.menu__link}>Skills</a>
+                        <a href="#" className={s.menu__link} onClick={onLinkClick}>Skills</a>
                     </li>
                     <li className={s.menu__item}>
-                        <a href="#" className={s.menu__link}>Projects</a>
+                        <a href="#" className={s.menu__link} onClick={onLinkClick}>Projects</a>
                     </li>
                     <li className={s.menu__item}>
-                        <a href="#" className={s.menu__link}>Contacts</a>
+                        <a href="#" className={s.menu__link} onClick={onLinkClick}>Contacts</a>
                     </li>
                 </ul>
             </nav>
             <div className={s.burger}>
-                <div className={`${s.burger__wrapper}`}>
-                    <div className={s.burger__item}></div>
-                    <div className={s.burger__item}></div>
-                    <div className={s.burger__item}></div>
+                <div className={`${s.burger__wrapper} ${isClickBurger ? s.click : ""}`} onClick={onBurgerClick}>
+                    <span className={`${s.burger__item} ${s.burger__item1}`}></span>
+                    <span className={`${s.burger__item} ${s.burger__item2}`}></span>
+                    <span className={`${s.burger__item} ${s.burger__item3}`}></span>
                 </div>
             </div>
         </>
